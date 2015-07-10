9c5a8f8a5a718e5aca66a0b26046ae93aef06f50
Eugene Skinner <eskinfrn@gmail.com>
2015-07-10 14:55:52 -0600

    Launch Page Almost Ready

--- public/js/launch.js
+++ public/js/launch.js
@@ -1,18 +1,33 @@
 var city = document.getElementById('city'),
     output = document.getElementById('output'),
-    weather = document.getElementById('press');
+    weather = document.getElementById('press'),
+    launch = document.getElementById('launch'),
+    video = document.getElementById('video');
+    video.style.visibility ="hidden"
 
 
 function getWeather(){
   orbit.get("http://api.openweathermap.org/data/2.5/weather?q={"+city.value+"}", function(){
     var data = JSON.parse(this.response);
     console.log(data)
-    output.innerHTML += "<p> Weather Description: "+(data.weather[0].description)+"</p>";
+    output.innerHTML += "<p> Weather Description: "+(data.weather[0].description)+"</p>"
+    output.innerHTML += "<p> Humidity: "+data.main.humidity+"%</p>"
+    output.innerHTML += "<p> Temperature: "+data.main.temp+"K</p>"
+    output.innerHTML += "<p> Wind Speed: "+data.wind.speed+"mph</p>"
+    output.innerHTML += "<p> Pressure: "+data.main.pressure+"pascals</p>"
   })
 }
 
+function liftoff(){
+  console.log(video.src)
+  video.style.visibility="visible";
+  video.src ="https://www.youtube.com/embed/zRGeLQkWuYk&autoplay=1";
+}
+
 weather.addEventListener('click',function(e){
-  console.log('bar')
   getWeather();
-  console.log('foo')
+})
+
+launch.addEventListener('click',function(e){
+  liftoff();
 })
