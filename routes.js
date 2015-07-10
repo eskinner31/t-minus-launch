453d766f64bbd864d72da5bab74991b97a304f32
Eugene Skinner <eskinfrn@gmail.com>
2015-07-10 11:45:18 -0600

    First Route Added

--- routes.js
+++ routes.js
@@ -4,8 +4,44 @@ var routes = require('routes')(),
     users = db.get('users'),
     qs = require('qs'),
     view = require('./view'),
-    mime = require('mime')
+    mime = require('mime'); 
 
+
+routes.addRoute('/home', (req, res, url) => {
+  res.setHeader('Content-Type', 'text/html')
+  if (req.method === "GET") {
+    var templates = view.render ('home', {})
+    res.end()
+  }
+  if (req.method === "POST") {
+    var acum = ''
+    req.on('data', function(chunk) {
+      acum += chunk
+    })
+    req.on('end', function(){
+      var data = qs.parse(acum)
+      users.findOne({userName : data.userName}, function(err, doc) {
+        if (err) {
+          res.writeHead(302, {'Location' : '/register'})
+          res.end()
+          return
+        }
+        users.findOne({userPassword : data.userPassword}, function (err, doc) {
+          if (err) {
+            res.writeHead(302, {'Location' : '/register'})
+            res.end()
+            return
+          }
+          req.session.put('userName', 'data.userName')
+          res.writeHead(302, {'Location' : 'home/weather'})
+          res.end()
+        })
+      })
+    })
+  }
+})
+
+module.exports = routes
 
 
 
